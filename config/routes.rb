c9689587bcbba7b18d30bba5ec9f62640e2d6178
geggit <ronbeli@hotmail.com>
2011-05-08 03:02:15 +0200

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
+  resources :users
 
-  get "users/new"
+#  get "users/new"
 
 #  get "pages/about"
 #  get "pages/home"
@@ -61,7 +62,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  
+
     root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
