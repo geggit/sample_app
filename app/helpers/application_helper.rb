e8a16e9f950793d8e3d4cf36274d62966b8999d6
geggit <ronbeli@hotmail.com>
2011-06-02 10:25:29 +0200

    rregllimteksti

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Return a title on a per-page basis.
   def title
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Tell-it"
     if @title.nil?
       base_title
     else
@@ -12,7 +12,7 @@ module ApplicationHelper
   
   
   def logo
-    image_tag("logo.png", :alt => "Sample App", :class => "round")
+    image_tag("logo.png", :alt => "Tell-it", :class => "round")
   end
 
 
